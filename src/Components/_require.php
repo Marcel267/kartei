67969f9a50ddc92e8beac4d339e2be0b0e0bae5b
Marcel Martinez Otano <m.otano@omsag.de>
2023-05-23 13:51:00 +0200

    Refactor modal to class component

--- src/Components/_require.php
+++ src/Components/_require.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../Entity/Kartei.php';
 require_once __DIR__ . '/../Entity/Freund.php';
 require_once __DIR__ . '/../Entity/Adresse.php';
+require_once __DIR__ . '/Modal.php';
 $_SERVER['url'] = 'http://localhost/workspace/schule';
 // $_SERVER['url'] = 'http://localhost/code';
 
--- src/Controller/edit_freund.php
+++ src/Controller/edit_freund.php
@@ -71,37 +71,19 @@ include('../Components/_header.php');
                 <?= $adresse->getStraße() ?><br>
             </div>
             <a href="<?= $_SERVER['url'] ?>/kartei/src/Controller/edit_adresse.php?adresseId=<?= $adresse->getId() ?>&freundId=<?= $freundId  ?>" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-            <button class="font-medium text-red-600 dark:text-red-500 hover:underline"
-            data-modal-target="popup-modal-<?= $adresse->getId() ?>" data-modal-toggle="popup-modal-<?= $adresse->getId() ?>">
+            <button class="font-medium text-red-600 dark:text-red-500 hover:underline" data-modal-target="popup-modal-<?= $adresse->getId() ?>" data-modal-toggle="popup-modal-<?= $adresse->getId() ?>">
                 Delete
             </button>
         </div>
 
-        <div id="popup-modal-<?= $adresse->getId() ?>" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative w-full max-w-md max-h-full">
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal-<?= $adresse->getId() ?>">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                    <div class="p-6 text-center">
-                        <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                        </svg>
-                        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                            Willst du wirklich die Adresse löschen?
-                        </h3>
-                        <a href="<?= $_SERVER['url'] ?>/kartei/src/Controller/delete_adresse.php?freundId=<?= $freundId ?>&adresseId=<?= $adresse->getId() ?>" data-modal-hide="popup-modal-<?= $adresse->getId() ?>" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                            Ja, löschen
-                        </a>
-                        <button data-modal-hide="popup-modal-<?= $adresse->getId() ?>" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Nein, Abbruch</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php } ?>
+    <?php
+        $modal = new Modal(
+            $adresse->getId(),
+            'Willst du wirklich die Adresse löschen?',
+            $_SERVER['url'] . '/kartei/src/Controller/delete_adresse.php?freundId=' . $freundId . '&adresseId=' . $adresse->getId()
+        );
+        echo $modal->render();
+    } ?>
 </div>
 <a href="<?= $_SERVER['url'] . '/kartei/src/Controller/add_adresse.php?freundId=' . $freundId ?>" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 inline-flex gap-2">
     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" data-darkreader-inline-fill="">
